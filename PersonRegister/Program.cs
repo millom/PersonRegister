d37529efe03a17a762695532c3ad11f3cc94be61
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-05-20 16:00:55 +0200

    Program: cleanup

--- PersonRegister/Program.cs
+++ PersonRegister/Program.cs
@@ -5,27 +5,84 @@ var register = new Register(new List<Person>());
 var doLoop = true;
 while (doLoop)
 {
-    Console.WriteLine("Vad vill du görra?");
-    Console.WriteLine("   A - Mata in en anställd?");
-    Console.WriteLine("   S - Titta på registret?");
-    Console.WriteLine("   Något annat - Avsluta?");
-    Console.Write("> ");
-    var command = Console.ReadLine();
-    switch(command.ToUpper())
+    PrintMenu();
+    string? command;
+    try
+    {
+        command = GetCommand();
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Bad Command, quit program");
+        doLoop = false;
+        continue;
+    }
+    switch (command.ToUpper())
     {
         case "A":
-            Console.WriteLine("Skriv in namn");
-            var name = Console.ReadLine();
-            Console.WriteLine("Skriv in lön");
-            var salary = double.Parse(Console.ReadLine().Replace(".", ","));
-            register.AddPerson(new Person(name, salary));
+            string? name = GetName();
+            double salary;
+            try
+            {
+                salary = GetSalary();
+            }
+            catch
+            {
+                Console.WriteLine();
+                continue;
+            }
+            var person = new Person(name, salary);
+            register.AddPerson(person);
+            Console.WriteLine($"Person <{person}> added");
             break;
         case "S":
-            register.ShowwRegister();
+            try
+            {
+                register.ShowwRegister();
+            }
+            catch (Exception)
+            {
+                continue;
+            }
             break;
         default:
             doLoop = false;
             break;
     }
     Console.WriteLine();
+}
+
+static void PrintMenu()
+{
+    Console.WriteLine("Vad vill du görra?");
+    Console.WriteLine("   A - Mata in en anställd?");
+    Console.WriteLine("   S - Titta på registret?");
+    Console.WriteLine("   Något annat - Avsluta?");
+    Console.Write("> ");
+}
+
+static string? GetName()
+{
+    Console.WriteLine("Skriv in namn");
+    var name = Console.ReadLine();
+    return name;
+}
+
+static double GetSalary()
+{
+    Console.WriteLine("Skriv in lön");
+    try
+    {
+        return double.Parse(Console.ReadLine().Replace(".", ","));
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Bad input for salary!");
+        throw;
+    }
+}
+
+static string? GetCommand()
+{
+    return Console.ReadLine();
 }
