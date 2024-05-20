9c333fa1d3fd4fcc4f2708ad4eea9fc470e8524a
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-05-20 15:23:50 +0200

    Program: Add class

--- PersonRegister/Program.cs
+++ PersonRegister/Program.cs
@@ -2,8 +2,30 @@
 
 var register = new Register(new List<Person>());
 
-while (true)
+var doLoop = true;
+while (doLoop)
 {
     Console.WriteLine("Vad vill du görra?");
-    Console.ReadLine();
+    Console.WriteLine("   A - Mata in en anställd?");
+    Console.WriteLine("   S - Titta på registret?");
+    Console.WriteLine("   Något annat - Avsluta?");
+    Console.Write("> ");
+    var command = Console.ReadLine();
+    switch(command.ToUpper())
+    {
+        case "A":
+            Console.WriteLine("Skriv in namn");
+            var name = Console.ReadLine();
+            Console.WriteLine("Skriv in lön");
+            var salary = double.Parse(Console.ReadLine().Replace(".", ","));
+            register.AddPerson(new Person(name, salary));
+            break;
+        case "S":
+            register.ShowwRegister();
+            break;
+        default:
+            doLoop = false;
+            break;
+    }
+    Console.WriteLine();
 }
